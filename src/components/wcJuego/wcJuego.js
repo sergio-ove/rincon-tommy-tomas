e89673f7a78f8ce7499589a02ec4dfda4ac4e9b3
SERGIOOVEP <sergio.ovejeropascual@gmail.com>
2024-07-28 20:26:44 +0200

    añado logica de juego

--- src/components/wcJuego/wcJuego.js
+++ src/components/wcJuego/wcJuego.js
@@ -10,25 +10,68 @@ export default defineComponent({
     components: {
       
     },
-
     data() {
         return {
-            gridItems: [
-                { name: 'sol', image: solImage },
-                { name: 'tractor', image: tractorImage },
-                { name: 'vaca', image: vacaImage },
-                { name: 'moto', image: motoImage },
-              ],
+          items: [
+            { name: 'sol', image: solImage, isFlipped: false, isMatched: false },
+            { name: 'arbol', image: motoImage, isFlipped: false, isMatched: false },
+            { name: 'sol', image: solImage, isFlipped: false, isMatched: false },
+            { name: 'arbol', image: motoImage, isFlipped: false, isMatched: false },
+            { name: 'tractor', image: tractorImage, isFlipped: false, isMatched: false },
+            { name: 'vaca', image: vacaImage, isFlipped: false, isMatched: false },
+            { name: 'tractor', image: tractorImage, isFlipped: false, isMatched: false },
+            { name: 'vaca', image: vacaImage, isFlipped: false, isMatched: false },
+          ],
+          firstCardIndex: null,
+          secondCardIndex: null,
           message: '',
         };
       },
+      computed: {
+        shuffledItems() {
+          return this.shuffle([...this.items]);
+        },
+      },
       methods: {
-        checkItem(item) {
-          if (item.name === 'sol') {
-            this.message = '¡Encontraste el sol!';
-          } else {
-            this.message = 'Sigue buscando...';
+        shuffle(array) {
+          for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+          }
+          return array;
+        },
+        flipCard(index) {
+          if (this.firstCardIndex === null) {
+            this.firstCardIndex = index;
+            this.items[index].isFlipped = true;
+          } else if (this.secondCardIndex === null && this.firstCardIndex !== index) {
+            this.secondCardIndex = index;
+            this.items[index].isFlipped = true;
+            this.checkMatch();
           }
         },
-      }
+        checkMatch() {
+          const firstCard = this.items[this.firstCardIndex];
+          const secondCard = this.items[this.secondCardIndex];
+          
+          if (firstCard.name === secondCard.name) {
+            this.items[this.firstCardIndex].isMatched = true;
+            this.items[this.secondCardIndex].isMatched = true;
+            this.resetTurn();
+            if (this.items.every(item => item.isMatched)) {
+              this.message = '¡Has encontrado todas las parejas!';
+            }
+          } else {
+            setTimeout(() => {
+              this.items[this.firstCardIndex].isFlipped = false;
+              this.items[this.secondCardIndex].isFlipped = false;
+              this.resetTurn();
+            }, 1000);
+          }
+        },
+        resetTurn() {
+          this.firstCardIndex = null;
+          this.secondCardIndex = null;
+        },
+      },
 });
--- src/components/wcJuego/wcJuego.vue
+++ src/components/wcJuego/wcJuego.vue
@@ -1,9 +1,21 @@
 <template>
     <div class="game-container">
-      <h1>Encuentra el Sol</h1>
+      <h1>Juego de Memoria: Encuentra las Parejas</h1>
       <div class="grid">
-        <div v-for="(item, index) in gridItems" :key="index" class="grid-item" @click="checkItem(item)">
-          <img :src="item.image" :alt="item.name" />
+        <div 
+          v-for="(item, index) in shuffledItems" 
+          :key="index" 
+          class="grid-item" 
+          @click="flipCard(index)"
+        >
+          <div 
+            :class="['card', { 'is-flipped': item.isFlipped, 'is-matched': item.isMatched }]"
+          >
+            <div class="card-face card-front"></div>
+            <div class="card-face card-back">
+              <img :src="item.isFlipped || item.isMatched ? item.image : defaultImage" :alt="item.name" />
+            </div>
+          </div>
         </div>
       </div>
       <div v-if="message" class="message">{{ message }}</div>
@@ -28,25 +40,53 @@ h1 {
 .grid {
   display: flex;
   flex-wrap: wrap;
-  width: 300px;
+  width: 400px;
 }
 
 .grid-item {
   width: 100px;
   height: 100px;
   margin: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 2px solid #ccc;
-  border-radius: 10px;
-  background-color: #fff;
+  perspective: 1000px;
+}
+
+.card {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.6s;
   cursor: pointer;
 }
 
-.grid-item img {
-  max-width: 80px;
-  max-height: 80px;
+.card.is-flipped {
+  transform: rotateY(180deg);
+}
+
+.card-face {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card-front {
+  background-color: #eee;
+}
+
+.card-back {
+  transform: rotateY(180deg);
+}
+
+.card-back img {
+  max-width: 100%;
+  max-height: 100%;
 }
 
 .message {
